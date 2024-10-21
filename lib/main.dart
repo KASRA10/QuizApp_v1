c775cbb4b105509aa9ff7113730c8bf7c41f35a3
kasra10 <kasrasparrow@gmail.com>
2024-10-21 13:42:10 +0330

    Edit UI and Paddings And Extended Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,9 @@ class Quizzler extends StatelessWidget {
         backgroundColor: Colors.grey.shade900,
         body: const SafeArea(
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            padding: EdgeInsets.symmetric(
+              horizontal: 15.5,
+            ),
             child: QuizPage(),
           ),
         ),
@@ -33,68 +35,59 @@ class _QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        const Expanded(
-          flex: 5,
-          child: Padding(
-            padding: EdgeInsets.all(
-              10.0,
+        const Text(
+          'This is where the question text will go.',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 21.0,
+            color: Colors.white,
+          ),
+        ),
+        const Divider(
+          color: Colors.transparent,
+          height: 55.5,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            //The user picked true.
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.green,
+            padding: const EdgeInsets.symmetric(
+              vertical: 15.0,
+              horizontal: 120.0,
             ),
-            child: Center(
-              child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 21.0,
-                  color: Colors.white,
-                ),
-              ),
+          ),
+          child: const Text(
+            'True',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
             ),
           ),
         ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(
-              30.0,
-            ),
-            child: ElevatedButton(
-              onPressed: () {
-                //The user picked true.
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green,
-              ),
-              child: const Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
+        const Divider(
+          color: Colors.transparent,
+          height: 55.5,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            //The user picked false.
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            padding: const EdgeInsets.symmetric(
+              vertical: 15.0,
+              horizontal: 120.0,
             ),
           ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(
-              30.0,
-            ),
-            child: ElevatedButton(
-              onPressed: () {
-                //The user picked false.
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
-              ),
-              child: const Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
+          child: const Text(
+            'False',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
             ),
           ),
         ),
